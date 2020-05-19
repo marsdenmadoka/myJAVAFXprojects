61d7bb6a53494fa5f7af077312c5d7f9433a33ea
marsdenmadoka <marsmadoka98@gmail.com>
2020-05-19 15:54:17 +0300

    new files

--- JavaFX Canvas/src/sample/Controller.java
+++ JavaFX Canvas/src/sample/Controller.java
@@ -1,4 +1,34 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class Controller {
+
+    @FXML
+// The reference of inputText will be injected by the FXML loader
+    private TextField inputText;
+    // The reference of outputText will be injected by the FXML loader
+    @FXML
+    private TextArea outputText;
+    // location and resources will be automatically injected by the FXML loader
+    @FXML
+    private URL location;
+    @FXML
+    private ResourceBundle resources;
+    // Add a public no-args constructor
+    @FXML
+    private void initialize()
+    {
+    }
+    @FXML
+    private void printOutput()
+    {
+        outputText.setText(inputText.getText());
+    }
 }
+
